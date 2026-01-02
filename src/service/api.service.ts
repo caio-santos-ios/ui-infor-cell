c6664d5d1d075c15424f6039c65f234d44676758
Caio Santos <caiosantos.dev@outlook.com>
2026-01-02 20:47:30 -0300

    fix

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // export const uriBase = 'http://localhost:5097'
-// export const uriBase = 'https://api.erpmais.online'
-export const uriBase = process.env.NEXT_PUBLIC_API_URL;
+export const uriBase = 'https://api.erpmais.online'
+// export const uriBase = process.env.NEXT_PUBLIC_API_URL;
 export const baseURL = `${uriBase}/api`;
 
 export const api = axios.create({
