3b75fea99c27b6583d61e04d8acd49d18bb2788d
Caio Santos <caiosantos.dev@outlook.com>
2026-03-22 17:04:50 -0300

    feat: add url da api

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const uriBase = process.env.NEXT_PUBLIC_API_URL;
-export const uriBase = "https://api-slim-qa.up.railway.app";
+export const uriBase = "https://api-telemovvi-qa.up.railway.app";
 export const baseURL = `${uriBase}/api`;
 
 export const api = axios.create({
